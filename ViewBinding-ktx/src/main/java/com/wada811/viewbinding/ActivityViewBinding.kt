63a16293c7d4724990ab4ff9b3dffd4f6cd7daf5
wada811 <at.wada811@gmail.com>
2021-03-07 23:53:15 +0900

    Fix memory leak

--- ViewBinding-ktx/src/main/java/com/wada811/viewbinding/ActivityViewBinding.kt
+++ ViewBinding-ktx/src/main/java/com/wada811/viewbinding/ActivityViewBinding.kt
@@ -8,16 +8,14 @@ import androidx.fragment.app.FragmentActivity
 import androidx.viewbinding.ViewBinding
 
 inline fun <reified T : ViewBinding> FragmentActivity.viewBinding(
-    crossinline bind: (View) -> T = { T::class.java.getMethod("bind", View::class.java).invoke(null, it) as T }
-): Lazy<T> = object : Lazy<T> {
-    private var binding: T? = null
-    override fun isInitialized(): Boolean = binding != null
-    override val value: T
-        get() = binding ?: bind(getContentView()).also { binding = it }
-
-    private fun FragmentActivity.getContentView(): View {
-        return checkNotNull(findViewById<ViewGroup>(android.R.id.content).getChildAt(0)) {
+    crossinline bind: (View) -> T = {
+        T::class.java.getMethod("bind", View::class.java).invoke(null, it) as T
+    }
+): Lazy<T> = lazy(LazyThreadSafetyMode.NONE) {
+    val getContentView: FragmentActivity.() -> View = {
+        checkNotNull(findViewById<ViewGroup>(android.R.id.content).getChildAt(0)) {
             "Call setContentView or Use Activity's secondary constructor passing layout res id."
         }
     }
+    bind(getContentView())
 }
