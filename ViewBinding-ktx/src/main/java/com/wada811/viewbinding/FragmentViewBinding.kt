7047836c18b3ef2e0f191b4750fb7d1f7d2b0aba
wada811 <at.wada811@gmail.com>
2021-04-22 23:32:25 +0900

    Separate viewBinding method

--- ViewBinding-ktx/src/main/java/com/wada811/viewbinding/FragmentViewBinding.kt
+++ ViewBinding-ktx/src/main/java/com/wada811/viewbinding/FragmentViewBinding.kt
@@ -6,14 +6,29 @@ import androidx.viewbinding.ViewBinding
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
-inline fun <reified T : ViewBinding> Fragment.viewBinding(
-    crossinline bind: (View) -> T = { T::class.java.getMethod("bind", View::class.java).invoke(null, it) as T }
-): ReadOnlyProperty<Fragment, T> = object : ReadOnlyProperty<Fragment, T> {
-    @Suppress("UNCHECKED_CAST")
-    override fun getValue(thisRef: Fragment, property: KProperty<*>): T {
-        (requireView().getTag(property.name.hashCode()) as? T)?.let { return it }
-        return bind(requireView()).also {
-            requireView().setTag(property.name.hashCode(), it)
+inline fun <reified T : ViewBinding> Fragment.viewBinding(): ReadOnlyProperty<Fragment, T> {
+    return object : ReadOnlyProperty<Fragment, T> {
+        @Suppress("UNCHECKED_CAST")
+        override fun getValue(thisRef: Fragment, property: KProperty<*>): T {
+            (requireView().getTag(property.name.hashCode()) as? T)?.let { return it }
+            val bind: (View) -> T = {
+                T::class.java.getMethod("bind", View::class.java).invoke(null, it) as T
+            }
+            return bind(requireView()).also {
+                requireView().setTag(property.name.hashCode(), it)
+            }
+        }
+    }
+}
+
+fun <T : ViewBinding> Fragment.viewBinding(bind: (View) -> T): ReadOnlyProperty<Fragment, T> {
+    return object : ReadOnlyProperty<Fragment, T> {
+        @Suppress("UNCHECKED_CAST")
+        override fun getValue(thisRef: Fragment, property: KProperty<*>): T {
+            (requireView().getTag(property.name.hashCode()) as? T)?.let { return it }
+            return bind(requireView()).also {
+                requireView().setTag(property.name.hashCode(), it)
+            }
         }
     }
 }
