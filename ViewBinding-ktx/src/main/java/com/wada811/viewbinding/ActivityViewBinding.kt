de5430322d7873f57410467c41573f2e1d7db7e2
wada811 <at.wada811@gmail.com>
2021-04-22 23:42:31 +0900

    Separate viewBinding method

--- ViewBinding-ktx/src/main/java/com/wada811/viewbinding/ActivityViewBinding.kt
+++ ViewBinding-ktx/src/main/java/com/wada811/viewbinding/ActivityViewBinding.kt
@@ -7,15 +7,27 @@ import android.view.ViewGroup
 import androidx.fragment.app.FragmentActivity
 import androidx.viewbinding.ViewBinding
 
-inline fun <reified T : ViewBinding> FragmentActivity.viewBinding(
-    crossinline bind: (View) -> T = {
-        T::class.java.getMethod("bind", View::class.java).invoke(null, it) as T
-    }
-): Lazy<T> = lazy(LazyThreadSafetyMode.NONE) {
-    val getContentView: FragmentActivity.() -> View = {
-        checkNotNull(findViewById<ViewGroup>(android.R.id.content).getChildAt(0)) {
-            "Call setContentView or Use Activity's secondary constructor passing layout res id."
+inline fun <reified T : ViewBinding> FragmentActivity.viewBinding(): Lazy<T> {
+    return lazy(LazyThreadSafetyMode.NONE) {
+        val bind: (View) -> T = {
+            T::class.java.getMethod("bind", View::class.java).invoke(null, it) as T
         }
+        val getContentView: FragmentActivity.() -> View = {
+            checkNotNull(findViewById<ViewGroup>(android.R.id.content).getChildAt(0)) {
+                "Call setContentView or Use Activity's secondary constructor passing layout res id."
+            }
+        }
+        bind(getContentView())
     }
-    bind(getContentView())
+}
+
+fun <T : ViewBinding> FragmentActivity.viewBinding(bind: (View) -> T): Lazy<T> {
+    return lazy(LazyThreadSafetyMode.NONE) {
+        val getContentView: FragmentActivity.() -> View = {
+            checkNotNull(findViewById<ViewGroup>(android.R.id.content).getChildAt(0)) {
+                "Call setContentView or Use Activity's secondary constructor passing layout res id."
+            }
+        }
+        bind(getContentView())
+    }
 }
