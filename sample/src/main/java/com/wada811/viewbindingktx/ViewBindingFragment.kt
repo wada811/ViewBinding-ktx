a0c3f6db20632ae9aec6f60799cea1589910c0c9
wada811 <at.wada811@gmail.com>
2021-11-09 16:07:28 +0900

    Update sample

--- sample/src/main/java/com/wada811/viewbindingktx/ViewBindingFragment.kt
+++ sample/src/main/java/com/wada811/viewbindingktx/ViewBindingFragment.kt
@@ -3,19 +3,20 @@ package com.wada811.viewbindingktx
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import com.wada811.viewbinding.viewBinding
+import com.wada811.viewbinding.withBinding
 import com.wada811.viewbindingktx.databinding.ViewBindingFragmentBinding
 import com.wada811.viewbindingktx.databinding.ViewStubBinding
 
 class ViewBindingFragment : Fragment(R.layout.view_stub) {
-    private val viewStubBinding: ViewStubBinding by viewBinding()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewStubBinding.viewStub.setOnInflateListener { _, inflated ->
-            val binding = ViewBindingFragmentBinding.bind(inflated)
-            binding.text.text = requireArguments().getString(EXTRA_TEXT)
+        withBinding<ViewStubBinding> { viewStubBinding ->
+            viewStubBinding.viewStub.setOnInflateListener { _, inflated ->
+                val binding = ViewBindingFragmentBinding.bind(inflated)
+                binding.text.text = requireArguments().getString(EXTRA_TEXT)
+            }
+            viewStubBinding.viewStub.inflate()
         }
-        viewStubBinding.viewStub.inflate()
     }
 
     companion object {
