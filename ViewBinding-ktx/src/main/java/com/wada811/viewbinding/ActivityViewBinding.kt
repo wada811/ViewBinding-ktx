5b7a8a6af047d510de043c98fefe5cec4d6eff29
wada811 <at.wada811@gmail.com>
2021-11-10 13:28:53 +0900

    Add withBinding without reflection

--- ViewBinding-ktx/src/main/java/com/wada811/viewbinding/ActivityViewBinding.kt
+++ ViewBinding-ktx/src/main/java/com/wada811/viewbinding/ActivityViewBinding.kt
@@ -8,16 +8,8 @@ import androidx.fragment.app.FragmentActivity
 import androidx.viewbinding.ViewBinding
 
 inline fun <reified T : ViewBinding> FragmentActivity.viewBinding(): Lazy<T> {
-    return lazy(LazyThreadSafetyMode.NONE) {
-        val bind: (View) -> T = {
-            T::class.java.getMethod("bind", View::class.java).invoke(null, it) as T
-        }
-        val getContentView: FragmentActivity.() -> View = {
-            checkNotNull(findViewById<ViewGroup>(android.R.id.content).getChildAt(0)) {
-                "Call setContentView or Use Activity's secondary constructor passing layout res id."
-            }
-        }
-        bind(getContentView())
+    return viewBinding {
+        T::class.java.getMethod("bind", View::class.java).invoke(null, it) as T
     }
 }
 
@@ -32,10 +24,14 @@ fun <T : ViewBinding> FragmentActivity.viewBinding(bind: (View) -> T): Lazy<T> {
     }
 }
 
-inline fun <reified T : ViewBinding> FragmentActivity.withBinding(withBinding: (binding: T) -> Unit) {
-    val bind: (View) -> T = {
+inline fun <reified T : ViewBinding> FragmentActivity.withBinding(noinline withBinding: (binding: T) -> Unit) {
+    withBinding({
         T::class.java.getMethod("bind", View::class.java).invoke(null, it) as T
-    }
+    }, withBinding)
+}
+
+
+fun <T : ViewBinding> FragmentActivity.withBinding(bind: (View) -> T, withBinding: (binding: T) -> Unit) {
     val getContentView: FragmentActivity.() -> View = {
         checkNotNull(findViewById<ViewGroup>(android.R.id.content).getChildAt(0)) {
             "Call setContentView or Use Activity's secondary constructor passing layout res id."
--- ViewBinding-ktx/src/main/java/com/wada811/viewbinding/FragmentViewBinding.kt
+++ ViewBinding-ktx/src/main/java/com/wada811/viewbinding/FragmentViewBinding.kt
@@ -7,17 +7,8 @@ import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
 inline fun <reified T : ViewBinding> Fragment.viewBinding(): ReadOnlyProperty<Fragment, T> {
-    return object : ReadOnlyProperty<Fragment, T> {
-        @Suppress("UNCHECKED_CAST")
-        override fun getValue(thisRef: Fragment, property: KProperty<*>): T {
-            (requireView().getTag(R.id.view_binding_tag) as? T)?.let { return it }
-            val bind: (View) -> T = {
-                T::class.java.getMethod("bind", View::class.java).invoke(null, it) as T
-            }
-            return bind(requireView()).also {
-                requireView().setTag(R.id.view_binding_tag, it)
-            }
-        }
+    return viewBinding {
+        T::class.java.getMethod("bind", View::class.java).invoke(null, it) as T
     }
 }
 
@@ -33,11 +24,14 @@ fun <T : ViewBinding> Fragment.viewBinding(bind: (View) -> T): ReadOnlyProperty<
     }
 }
 
-inline fun <reified T : ViewBinding> Fragment.withBinding(withBinding: (binding: T) -> Unit) {
+inline fun <reified T : ViewBinding> Fragment.withBinding(noinline withBinding: (binding: T) -> Unit) {
+    withBinding({
+        T::class.java.getMethod("bind", View::class.java).invoke(null, it) as T
+    }, withBinding)
+}
+
+fun <T : ViewBinding> Fragment.withBinding(bind: (View) -> T, withBinding: (binding: T) -> Unit) {
     view?.let { view ->
-        val bind: (View) -> T = {
-            T::class.java.getMethod("bind", View::class.java).invoke(null, it) as T
-        }
         val binding = bind(view)
         withBinding(binding)
     }
